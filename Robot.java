77d19f3e6d8bc4e5d998f8d8393b7319779b23f5
cpblack <caleb.paul.black@gmail.com>
2016-10-26 15:50:58 -0600

    Update Robot.java

--- Robot.java
+++ Robot.java
@@ -1,11 +1,17 @@
 
 package org.usfirst.frc.team5974.robot;
 
-import edu.wpi.first.wpilibj.IterativeRobot;
-import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-
+import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.CameraServer; //The Camera\
+import edu.wpi.first.wpilibj.DigitalInput; //Digital inputs
+import edu.wpi.first.wpilibj.Timer; // The Timer
+import edu.wpi.first.wpilibj.IterativeRobot; //Guess
+import edu.wpi.first.wpilibj.Joystick; //The Controller
+import edu.wpi.first.wpilibj.VictorSP; //The Motor Controller
+import edu.wpi.first.wpilibj.interfaces.Accelerometer;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard; //The Dashboard
+import edu.wpi.first.wpilibj.ADXL362; //The Accelerometer
+import edu.wpi.first.wpilibj.ADXRS450_Gyro;
 
 public class Robot extends IterativeRobot {
     
