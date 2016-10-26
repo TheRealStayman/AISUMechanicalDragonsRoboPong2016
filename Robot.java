86449b3ca038b3b15f62e6fb4934620b97b0b23b
clavierkid <carter@CKMBA.local>
2016-10-26 15:53:29 -0600

    Variables
    Defined joystick variables

--- Robot.java
+++ Robot.java
@@ -6,6 +6,13 @@ import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
+//Defining Variables
+RobotDrive myDrive;
+Joystick controller;
+double joy1X  //Joystick 1 X,Y
+double joy1Y
+double joy2X  //Joystick 2 X,Y
+double joy2Y
 
 public class Robot extends IterativeRobot {
     
