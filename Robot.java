e90451b826a076b54251b115ca9d34fbc5234850
TheRealStayman <thomasstay@gmail.com>
2016-11-02 16:15:58 -0600

    Update Robot.java

--- Robot.java
+++ Robot.java
@@ -1,13 +1,7 @@
 
 package org.usfirst.frc.team5974.robot;
 
-<<<<<<< HEAD
-import edu.wpi.first.wpilibj.IterativeRobot;
-import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-=======
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.CameraServer; //The Camera\
 import edu.wpi.first.wpilibj.DigitalInput; //Digital inputs
@@ -19,18 +13,41 @@ import edu.wpi.first.wpilibj.interfaces.Accelerometer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard; //The Dashboard
 import edu.wpi.first.wpilibj.ADXL362; //The Accelerometer
 import edu.wpi.first.wpilibj.ADXRS450_Gyro;
->>>>>>> origin/master
 
 
 
 public class Robot extends IterativeRobot {
     //Defining Variables
+    RobotDrive myDrive;
     Joystick controller;
-    RobotDrive drive;
-    double[] joy1;
-    double[] joy2;
-    joy1 = new double[2];
-    joy2 = new double[2];
+    double joy1X;  //Joystick 1 X,Y
+    double joy1Y;
+    double joy2X;  //Joystick 2 X,Y
+    double joy2Y;
+    //Jed's Not Victor
+    VictorSP upperLeftMotor = new VictorSP(0); //Upper left Motor
+    VictorSP lowerRightMotor = new VictorSP(1); //Lower Right Motor
+    VictorSP upperRightMotor = new VictorSP(3); //Upper Right Motor
+    VictorSP lowerLeftMotor = new VictorSP(2); //Lower Left Motor   
+    
+    
+    public void updateStuff() {
+        joy1X = controller.getRawAxis(0); //left joystick X
+    	joy1Y = controller.getRawAxis(1); //left joystick Y
+    	joy2X = controller.getRawAxis(4); //right joystick
+    	joy2Y = controller.getRawAxis(5);
+    	deadZone(); //See below
+    }
+    public void deadZone() {
+        double deadZoneDouble = 0.15;
+    	if (joy1Y <= Math.abs(deadZoneDouble)) { //For joysticks
+    		joy1Y = 0;
+    	}
+    	if (joy2Y <= Math.abs(deadZoneDouble)) { //For joysticks
+    		joy2Y = 0;
+    	}
+    
+    }
 	
     public void robotInit() {
     	
@@ -53,28 +70,10 @@ public class Robot extends IterativeRobot {
     
     
     public void testPeriodic() {
-    	
+    	upperLeftMotor.set(joy1Y); //Control the left motors with the first joystick
+	lowerLeftMotor.set(joy1Y);
+	upperRightMotor.set(joy2Y); //Control the right motors with the second joystick
+	lowerRightMotor.set(joy2Y);
     }
-    public void updateStuff() {
-        joy1[0] = controller.getRawAxis(0); //left joystick X
-    	joy1[1] = controller.getRawAxis(1); //left joystick Y
- //   	leftTrigger = controller.getRawAxis(2); //ball picker-upper Motors
-  //  	rightTrigger = controller.getRawAxis(3);
-    	joy2[0] = controller.getRawAxis(4); //right joystick
-    	joy2[1] = controller.getRawAxis(5);
-//    	leftBumper = controller.getRawButton(5); //Speed Boost
-  //  	xButton = controller.getRawButton(3);
-    //	yButton = controller.getRawButton(4);
-    	deadZone(); //See below
-    }
-    public void deadZone() {
-        double deadZoneDouble = 0.15;
-    	if (joy1[1] <= Math.abs(deadZoneDouble)) { //For joysticks
-    		joy1[1] = 0;
-    	}
-    	if (joy1[1] <= Math.abs(deadZoneDouble)) { //For joysticks
-    		joy1[1] = 0;
-    	}
     
-    }
 }
